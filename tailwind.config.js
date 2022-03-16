507dc0d30e850d78b4726fd8926a7a55037bf77b
Yuvaan Mutreja <67225353+Yuvaan16@users.noreply.github.com>
2022-03-16 10:52:40 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      colors: {
+        'space-cadet': '#2b2d42ff;',
+        'manatee': '#aab5c9ff;',
+        'alice-blue': '#edf2f4ff;',
+        'imperial-red': '#ef233cff;',
+        'amaranth-red': '#d90429ff;',
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", 'sans-serif'],
+      },
+    },
+  }
